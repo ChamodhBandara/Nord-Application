531b8a61d5b085b35228b7f2af672227b3c7662d
lakshan <lakshanchanaka34@gmail.com>
2025-09-18 14:59:22 +0530

    Update server port and database credentials in application properties; modify CORS configuration for security settings.

--- Node_Holdings_LMS/Backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ Node_Holdings_LMS/Backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -95,12 +95,12 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5175"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/", configuration);
+        source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 }
